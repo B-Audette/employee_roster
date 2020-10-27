bcf00586d0426bd250a5b6e9fcbcda92ad3532a4
B-Audette <b.audette015@gmail.com>
2020-10-27 12:46:58 -0500

    fixed schema.sql with correct table values

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE employeeRosterdb;
 
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30),
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
@@ -14,16 +14,14 @@ CREATE TABLE role (
   salary DECIMAL,
   -- reference department employee belongs to
   department_id INT
-  
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  --refernce role employee has
-  role_id INT,
-  
+  -- refernce role employee has
+  role_id INT
 );
 
 SELECT * from department;
@@ -36,6 +34,5 @@ VALUES ('Ash'), ('Gary'), ('Red'), ('Leon');
 INSERT INTO role (title, salary, department_ID)
 VALUES ('Fire', 1000, 1), ('Water', 50000, 2), ('Electric', 60000, 3), ('Grass', 60000, 4);
 
-INSERT INTO employee (first_name, last_name, role_ID)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES  ('Char', 'mander', 1), ('Pika', 'chu', 2), ('Bulba', 'saur', 3), ('Squir', 'tle', 4);
-
