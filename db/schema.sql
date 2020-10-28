6f7246ba2ff0d2a41d2e04b0fd0c054d988c3694
B-Audette <b.audette015@gmail.com>
2020-10-27 22:26:33 -0500

    working view/add database in index.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employeeRosterdb;
-CREATE DATABASE employeeRosterdb;
+DROP DATABASE IF EXISTS employeerosterdb;
+CREATE DATABASE employeerosterdb;
 
-USE employeeRosterdb;
+USE employeerosterdb;
 
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,15 +24,15 @@ CREATE TABLE employee (
   role_id INT
 );
 
-SELECT * from department;
-SELECT * from role;
-SELECT * from employee;
-
 INSERT INTO department (name)
-VALUES ('Ash'), ('Gary'), ('Red'), ('Leon');
+VALUES ('Red'), ('Gary'), ('Ash'), ('Leon');
 
 INSERT INTO role (title, salary, department_ID)
 VALUES ('Fire', 1000, 1), ('Water', 50000, 2), ('Electric', 60000, 3), ('Grass', 60000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES  ('Char', 'mander', 1), ('Pika', 'chu', 2), ('Bulba', 'saur', 3), ('Squir', 'tle', 4);
+VALUES  ('Char', 'mander', 1), ('Squir', 'tle', 2), ('Pika', 'chu', 3), ('Bulba', 'saur', 4);
+
+SELECT * from department;
+SELECT * from role;
+SELECT * from employee;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { prompt } = require("inquirer");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 require("console.table")
@@ -10,7 +11,7 @@ const connection = mysql.createConnection({
   user: "root",
 
   password: "rootroot",
-  database: "employeeRosterdb"
+  database: "employeerosterdb"
 });
 
 // set up the connection and gather in the functions that will use the connection here to access the DB
@@ -23,35 +24,34 @@ connection.connect(function (err) {
  // ask user questions -- ideally getting the lists from the returned responses that are set to variables as my choice lists.
 async function menu() {
      inquirer .prompt({
-            name: "menuOptions",
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-                {name:"View Departments",
-                value: "viewDept",
-                },
-                {name:"Add Department",
-                value: "addDept",
-                },
-                {name:"View Roles",
-                value: "viewRoles",
-                },
-                {name:"Add Roles",
-                value: "addRoles",
+        name: "menuOptions",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            {name:"View Departments",
+            value: "viewDept",
             },
-                {name:"View Employees",
-                value: "viewEmployees",
-                },
-                {name:"Add Employee",
-                value: "AddEmployee",
-                },
-                {name:"Exit",
-                value: "exit",
+            {name:"Add Department",
+            value: "addDept",
+            },
+            {name:"View Roles",
+            value: "viewRoles",
+            },
+            {name:"Add Roles",
+            value: "addRoles",
+            },
+            {name:"View Employees",
+            value: "viewEmployees",
+            },
+            {name:"Add Employee",
+            value: "AddEmployee",
+            },
+            {name:"Exit",
+            value: "exit",
             }
-            ]
-        }).then(function(response) {
-            console.log(response)
-            switch(response) {
+        ]}).then(function(data) {
+            console.log(data)
+            switch(data.menuOptions) {
                 case "viewDept":
                     return readDept();
                 case "addDept":
@@ -70,30 +70,65 @@ async function menu() {
         })
 }
         
-
-
          
-// function to read the departments  so we can get the data to set to variable?
 async function readDept() {
     console.log("Reading all departments ... \n");
-    connection.query("SELECT * from department", function(err, res) {
+    let departments = await connection.query("SELECT * from department", function(err, res) {
     if (err) throw err;
-    printTable(department)
- 
+    let departments = res.map(({name}) => name)
+    console.table(departments)
+ menu();
 })
-menu();
 }
 
 async function addDept() {
-    console.log("Reading all departments ... \n");
-    connection.query("SELECT * from department", function(err, res) {
+    console.log("Preparing to add department ... \n");
+    let department = await prompt([
+        {
+            name: "name",
+            message: "What is the name of the department?"
+        }
+    ])
+    await connection.query("INSERT INTO department SET ?", department)
+    console.log(`Added ${department.name} to employeerosterdb`);
+    menu();
+}
+
+async function readRoles() {
+    console.log("Reading all roles ... \n");
+    let roles = await connection.query("SELECT * from role", function(err, res) {
     if (err) throw err;
-     //get response and set it to a variable
-     let departments = department.map( ({id, name }) => ({
-            name,
-            value:id
-     }));
- 
+    let roles = res.map(({title}) => title)
+    console.table(roles)
+ menu();
 })
 }
+
+async function addRoles() {
+    console.log("Preparing to add new role ... \n");
+    let departments = await connection.query("SELECT * from department", function(err, res) {
+        if (err) throw err;
+        return res.map(({name}) => name)})
+        console.table(departments)
+    let role = await prompt([
+        {
+            name: "title",
+            message: "What is the title for this role?"
+        },
+        {
+            name: "salary",
+            message: "What salary will this role make?"
+        },
+        {
+            type: "list",
+            name: "department_id",
+            message: "What department will this role be assigned to?",
+            choices: departments
+        }])
+    await connection.query("INSERT INTO role SET ?", role)
+    console.log(`Added ${role.name} to employeerosterdb`);
+    
+    menu();
+}
+
 
